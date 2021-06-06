446f5ff94b1d26478598844fa1b0506111c02503
Isaiah-1289 <85441307+Isaiah-1289@users.noreply.github.com>
2021-06-06 17:56:19 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Data Analyst Training and Internship</h1>
+      <p>
+        Data Analyst Training And Internship Join us for the 2.5 month-long
+        intensive industrial training and internship. Get placed as a Data
+        Analyst or a Software Development Engineer.
+      </p>
+
+      <p>
+        The Goal of this Training and Internship program is to Advance your
+        programming skills and refine your ability to work with messy, complex
+        datasets. You’ll learn to manipulate and prepare data for analysis, and
+        create visualizations for data exploration.
+      </p>
+    </div>
+  );
+}
+export default Note;
